ce9a368a82787c33b81e25294a841e67268753f2
alesfluiz <alesfluiz@gmail.com>
2023-11-09 03:09:48 -0300

    Concluindo Desafio de POO da Dio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("99999-9999", "Lumia", "1111111111122222", 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Iphone iphone = new Iphone("98888-8888", "XS Pró Max", "54584654465465424424654", 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Telegram");
